ad15167c5bae9bccda0c261ea140da94f202411f
IvanKurikin <ivan.kurykin@innowise.com>
2025-06-02 22:55:46 +0300

    add new method to appointment service

--- InnoClinic.AppointmentsAPI/BLL/Interfaces/IAppointmentService.cs
+++ InnoClinic.AppointmentsAPI/BLL/Interfaces/IAppointmentService.cs
@@ -4,6 +4,7 @@ namespace BLL.Interfaces;
 
 public interface IAppointmentService : IGenericService<AppointmentDto, CreateAppointmentDto, UpdateAppointmentDto>
 {
+    Task ApproveAppointmentAsync(Guid id, bool isApproved, CancellationToken cancellationToken = default);
     Task<AppointmentDto?> GetAppointmentByIdWithDetailsAsync(Guid id, CancellationToken cancellationToken = default);
     Task<IReadOnlyCollection<AppointmentDto>> GetAppointmentsWithDetailsAsync(CancellationToken cancellationToken = default);
 }
