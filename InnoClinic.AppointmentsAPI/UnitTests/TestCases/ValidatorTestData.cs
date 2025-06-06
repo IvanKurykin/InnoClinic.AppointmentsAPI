ed5e5ab935de5b61be018cb03af21b918021be0c
IvanKurikin <ivan.kurykin@innowise.com>
2025-06-07 01:04:22 +0300

    solve issues

--- InnoClinic.AppointmentsAPI/UnitTests/TestCases/ValidatorTestData.cs
+++ InnoClinic.AppointmentsAPI/UnitTests/TestCases/ValidatorTestData.cs
@@ -1,10 +1,7 @@
 ﻿namespace UnitTests.TestCases;
 
-public class ValidatorTestData
+public static class ValidatorTestData
 {
-    public static Guid TestId = Guid.NewGuid();
-    public static Guid EmptyId = Guid.Empty;
-
     public const string PatientIdRequired = "Patient ID must not be empty.";
     public const string DoctorIdRequired = "Doctor ID must not be empty.";
     public const string ServiceIdRequired = "Service ID must not be empty.";
@@ -12,9 +9,17 @@ public class ValidatorTestData
     public const string DateNotPast = "Appointment date cannot be in the past.";
     public const string TimeEndAfterStart = "End time must be after start time.";
     public const string CreatedByMaxLength = "'Created By' field cannot be longer than 255 characters.";
-
-    public static DateTime ValidDate = DateTime.UtcNow.AddDays(1).Date;
-    public static DateTime ValidTimeStart = new DateTime(10, 0, 0);
-    public static DateTime ValidTimeEnd = new DateTime(11, 0, 0);
     public const string ValidCreatedBy = "test@example.com";
+
+    private static readonly Guid _testId = Guid.NewGuid();
+    private static readonly Guid _emptyId = Guid.Empty;
+    private static readonly DateTime _validDate = DateTime.UtcNow.AddDays(1).Date;
+    private static readonly DateTime _validTimeStart = DateTime.Today.AddHours(10);
+    private static readonly DateTime _validTimeEnd = DateTime.Today.AddHours(11);
+
+    public static Guid TestId => _testId;
+    public static Guid EmptyId => _emptyId;
+    public static DateTime ValidDate => _validDate;
+    public static DateTime ValidTimeStart => _validTimeStart;
+    public static DateTime ValidTimeEnd => _validTimeEnd;
 }
